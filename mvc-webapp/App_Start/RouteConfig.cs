b8eb3afd69fffaab13aa161038ae2e712bcda9ed
Davehax <dave.21.65@gmail.com>
2018-04-11 16:56:18 +1000

    Up to "Adding a new field" in the tutorial

--- mvc-webapp/App_Start/RouteConfig.cs
+++ mvc-webapp/App_Start/RouteConfig.cs
@@ -16,7 +16,17 @@ namespace mvc_webapp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{message}/{numTimes}"
             );
         }
     }
---
+++ mvc-webapp/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_webapp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string message, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + message;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
