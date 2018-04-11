69eb1ec823e74c30be94c7166a2b0dd6b7be75eb
Davehax <dave.21.65@gmail.com>
2018-04-11 17:57:47 +1000

    Adding validation

--- mvc-webapp/Models/Movie.cs
+++ mvc-webapp/Models/Movie.cs
@@ -7,14 +7,26 @@ namespace mvc_webapp.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(maximumLength: 60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(maximumLength: 30)]
         public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(maximumLength: 5)]
         public string Rating { get; set; }
     }
 
